431f6f4023beed5f52549dd7a41e9a0547ae7a96
cultab <rroarck@gmail.com>
2021-12-03 10:45:38 +0200

    keybind

--- dmenu.c
+++ dmenu.c
@@ -370,6 +370,8 @@ keypress(XKeyEvent *ev)
 			break;
 		case XK_y: /* paste selection */
 		case XK_Y:
+		case XK_v: /* also with v,V */
+		case XK_V:
 			XConvertSelection(dpy, (ev->state & ShiftMask) ? clip : XA_PRIMARY,
 			                  utf8, utf8, win, CurrentTime);
 			return;
